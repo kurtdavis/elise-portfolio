c1fd88cf73840b8b81f5fc8bd0d128367b4554a5
Kurt Davis <kurt.davis@kewtec.com>
2024-01-22 15:00:49 -0500

    Adding missing Bakery file... not used yet.

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,4 @@
 import {createRouter, createWebHistory} from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 import AboutView from '../views/AboutView.vue'
 
 const router = createRouter({
---
+++ src/views/Bakery.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts" xmlns:h3="http://www.w3.org/1999/html">
+import { ref, computed } from 'vue'
+import PortfolioItem from '@/components/PortfolioItem.vue'
+</script>
+
+<template>
+  <h3 class="ma-2 pa-2">Various Baked Goods </h3>
+
+</template>
