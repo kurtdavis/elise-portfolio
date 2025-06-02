43851e94ff54518df490d4e400f5043f2c1e3ca7
kurtdavis <kurtdavis@users.noreply.github.com>
2025-06-02 02:18:53 +0000

    deploy: a1b0d7f7b407fa05fd1303167347e87f62030d53

---
+++ assets/Bakery-b30982ee.js
@@ -0,0 +1,1 @@
+import{i as e,o,e as a}from"./index-3cdd5b56.js";const s={class:"ma-2 pa-2"},_=e({__name:"Bakery",setup(t){return(n,r)=>(o(),a("h3",s,"Various Baked Goods "))}});export{_ as default};
